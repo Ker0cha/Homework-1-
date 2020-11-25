6222d7c5bfdf88b94db028d45ffc1af804d8a87f
Ker0cha <75005897+Ker0cha@users.noreply.github.com>
2020-11-25 12:56:37 +0700

    Add files via upload

---
+++ 1-3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Символьный
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            char c = Console.ReadLine()[0];
+            Console.WriteLine("Следующий символ: {0}", (char)(c + 1));
+            Console.ReadKey();
+
+        }
+    }
+}
