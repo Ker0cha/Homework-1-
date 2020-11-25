65f929110a49e89042d0b7811c0cfc3c2511a1e2
Ker0cha <75005897+Ker0cha@users.noreply.github.com>
2020-11-25 12:54:30 +0700

    Add files via upload

---
+++ 1-1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+ 
+namespace Ты_кто_такой
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Кто ты?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
